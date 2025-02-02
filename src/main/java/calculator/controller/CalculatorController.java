e20b19c05065ba71970b843c1d1d33f8cf9f0584
moongua404 <hogun.soongit@gmail.com>
2025-02-02 13:53:30 +0900

    feat(directory): MVC 파일 구조 도입
    - MVC 패턴에 맞게 파일 구조를 설정
    - model, view, controller, service, utils 패키지 및 클래스 생성
    - service는 interface와 구현체로 분리

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,26 @@
+package calculator.controller;
+
+import calculator.model.Expression;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+    InputView inputView;
+    OutputView outputView;
+    CalculatorService calculatorService;
+    Expression expression;
+
+    public CalculatorController(
+            InputView inputView, OutputView outputView, CalculatorService calculatorService, Expression expression
+    ) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.calculatorService = calculatorService;
+        this.expression = expression;
+    }
+
+    public void run() {
+
+    }
+}
