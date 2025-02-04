aa7503dfde5f6e2360b474f31ac2728a5741f5fc
moongua404 <hogun.soongit@gmail.com>
2025-02-04 21:30:25 +0900

    fix(View): input 부적절한 입력 처리
    - Console.readline함수를 try catch로 감싸서 예외처리

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -5,7 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String getExpression() {
-        System.out.println(MessageConstants.GUIDE_MESSAGE.getMessage());
-        return Console.readLine();
+        try {
+            System.out.println(MessageConstants.GUIDE_MESSAGE.getMessage());
+            return Console.readLine();
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("Something went wrong on input process.");
+        }
     }
 }
